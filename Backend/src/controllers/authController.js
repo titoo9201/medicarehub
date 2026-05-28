33fe704856555b58cc909b996a8561bc0ee94d95
titoo9201 <titoos675@gmail.com>
2026-05-28 14:40:00 +0530

    register controller bugs fixes

--- Backend/src/controllers/authController.js
+++ Backend/src/controllers/authController.js
@@ -1,9 +1,10 @@
 const jwt = require("jsonwebtoken");
 const User = require("../model/User");
+const { JWT_SECRET, JWT_EXPIRE } = require("../config/jwt");
 
 const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRE,
+  return jwt.sign({ id }, JWT_SECRET, {
+    expiresIn: JWT_EXPIRE,
   });
 };
 
